4a55817af2d67f2f33a8b536178778203f9ca81d
Dayan Rodriguez <microdev.rj@gmail.com>
2020-05-17 08:57:23 -0400

    final

--- app/src/main/java/com/microdevrj/flow_visualizer_example/MainActivity.kt
+++ app/src/main/java/com/microdevrj/flow_visualizer_example/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : PermissionsActivity(), MediaPlayer.OnPreparedListener {
 
     private fun initPlayer() {
         try {
-            mediaPlayer = MediaPlayer.create(this, R.raw.sample_song_4)
+            mediaPlayer = MediaPlayer.create(this, R.raw.sample_song_1)
             mediaPlayer.setOnPreparedListener(this)
         } catch (e: Exception) {
             Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
